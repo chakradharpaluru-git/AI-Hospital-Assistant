ae1b14f8617eaf709bce5f521cb20985e1f9ceea
chakri <chakripaluri3@gmail.com>
2026-08-10 12:06:42 +0530

    Use absolute path for Chroma vector store

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,3 +1,4 @@
+import os
 from functools import lru_cache
 
 from langchain_chroma import Chroma
@@ -5,8 +6,26 @@ from langchain_chroma import Chroma
 from rag.embeddings import get_embeddings
 
 
-VECTOR_PATH = "rag/vectorstore"
+# ==========================================================
+# PATH
+# ==========================================================
 
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+VECTOR_PATH = os.path.join(
+    BASE_DIR,
+    "rag",
+    "vectorstore"
+)
+
+
+# ==========================================================
+# CHROMA RETRIEVER
+# ==========================================================
 
 @lru_cache(maxsize=1)
 def get_retriever():
